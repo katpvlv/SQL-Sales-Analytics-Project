69fd78e1f8e22fa0f875d10fb29e97a88df1439b
katpvlv <81798251+katpvlv@users.noreply.github.com>
2023-06-14 19:06:19 +0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,52 +6,73 @@ from customers;
 with operation_tab as
 (
 select
-s.sales_person_id as sales_id,
-concat(e.first_name, ' ', e.last_name) as name,
-count(s.sales_id) as operations
+  s.sales_person_id as sales_id,
+  concat(e.first_name, ' ', e.last_name) as name,
+  count(s.sales_id) as operations
 from employees e
 join sales s 
-on e.employee_id = s.sales_person_id
+  on e.employee_id = s.sales_person_id
 group by s.sales_person_id, name 
 )
 , income_tab as
 (
 select 
-s.sales_person_id as sales_id, 
-sum(p.price * s.quantity) as income
+  s.sales_person_id as sales_id, 
+  sum(p.price * s.quantity) as income
 from sales s 
 join products p 
-on s.product_id = p.product_id
+  on s.product_id = p.product_id
 group by s.sales_person_id
 )
 select 
-name,
-operations,
-income
+  name,
+  operations,
+  income
 from operation_tab ot
 left join income_tab it
-on ot.sales_id = it.sales_id
+  on ot.sales_id = it.sales_id
 order by income desc
 ;
 
 with income_tab as
 (
 select 
-s.sales_person_id as sales_id, 
-sum(p.price * s.quantity) as income,
-count(s.sales_id) as operation
+  s.sales_person_id as sales_id, 
+  sum(p.price * s.quantity) as income,
+  count(s.sales_id) as operation
 from sales s 
 join products p 
-on s.product_id = p.product_id
+  on s.product_id = p.product_id
 group by s.sales_person_id
 )
 select
-concat(e.first_name, ' ', e.last_name) as name,
-round(it.income/it.operation) as average_income
+  concat(e.first_name, ' ', e.last_name) as name,
+  round(it.income/it.operation) as average_income
 from income_tab it
 join employees e
-on it.sales_id = e.employee_id
+  on it.sales_id = e.employee_id
 group by name, average_income
 having round(it.income/it.operation) < (select avg(income/operation) from income_tab)
 order by average_income
+;
+
+with weekday_income as
+(
+select
+	s.sales_person_id as sales_id,
+	to_char(s.sale_date, 'day') as weekday,
+	sum(p.price * s.quantity) as income
+from sales s 
+join products p 
+  on s.product_id = p.product_id
+group by s.sales_person_id, weekday
+)
+select 
+  concat(e.first_name, ' ', e.last_name) as name,
+  wi.weekday as weekday,
+  round(wi.income) as income
+from weekday_income wi
+join employees e
+  on wi.sales_id = e.employee_id
+order by weekday, name
 ;
